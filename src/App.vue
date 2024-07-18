7144d4d307335bb6ed9bf1a3d8797e0933681260
Samudra-Ekanayake <samudra2724@gmail.com>
2024-07-18 17:18:25 +0200

    M4

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
-<script setup>
-import  axios  from "axios";
-/*  import HelloWorld from './components/HelloWorld.vue' 
- */</script>
+<script>
+import axios from "axios";
+
+/*  import HelloWorld from './components/HelloWorld.vue'*/
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      url: 'http://127.0.0.1:8000',
+      projects: ''
+    }
+  },
+  mounted() {
+    axios
+      .get(`${this.url}/api/projects`)
+      .then(response => {
+        console.log(response);
+        this.projects = response.data.projects
+      })
+  },
+}
+</script>
 
 <template>
-  
+
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
